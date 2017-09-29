21e7b40fad450a0bb082856b5653b8e2edfe6b47
Julee117 <juliannne@gmail.com>
2017-09-29 03:21:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,6 @@
 var playlist = {};
 var playlist = {alicia: "Empire"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, {[artistName]:songTitle})
+}
